12fa9b08a8b2003a542e75b29bcc94251c0a5afa
nilsson <mansnilsson2001@hotmail.com>
2025-10-08 13:52:21 +0200

    add Exercise entity

---
+++ src/main/java/com/jerry/workoutapp/entity/Exercise.java
@@ -0,0 +1,57 @@
+package com.jerry.workoutapp.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Exercises")
+public class Exercise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "exercise_id", columnDefinition = "INTEGER")
+    private long exerciseId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "muscle_group")
+    private String muscleGroup;
+
+    @Column(name = "equipment")
+    private String equipment;
+
+    @Column(name = "source")
+    private String source;
+
+
+    public Exercise() {
+    }
+
+    public Exercise(String name, String description, String muscleGroup, String equipment, String source) {
+        this.name = name;
+        this.description = description;
+        this.muscleGroup = muscleGroup;
+        this.equipment = equipment;
+        this.source = source;
+    }
+
+    public long getExerciseId() { return exerciseId; }
+    public void setExerciseId(long exerciseId) { this.exerciseId = exerciseId; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public String getMuscleGroup() { return muscleGroup; }
+    public void setMuscleGroup(String muscleGroup) { this.muscleGroup = muscleGroup; }
+
+    public String getEquipment() { return equipment; }
+    public void setEquipment(String equipment) { this.equipment = equipment; }
+
+    public String getSource() { return source; }
+    public void setSource(String source) { this.source = source; }
+}
