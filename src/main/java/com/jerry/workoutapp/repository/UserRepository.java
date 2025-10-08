a357c3e69e154bbb6eb0ca2fcce9e04cc02318be
Jerry <you@example.com>
2025-10-08 17:01:07 +0200

    fixed dependency swagger and user repo

--- src/main/java/com/jerry/workoutapp/repository/UserRepository.java
+++ src/main/java/com/jerry/workoutapp/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.jerry.workoutapp.repository;
 
+import com.jerry.workoutapp.entity.Exercise;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserRepository {
+public interface UserRepository extends JpaRepository<Exercise, Long> {
 }
