0e91eb36c165508b8d0adb397e69d43afdcccb0f
Lillebravo <jerry.lundahl@hotmail.com>
2025-10-08 13:10:14 +0200

    Added user entity, dependencies and packages

---
+++ src/main/java/com/jerry/workoutapp/entity/User.java
@@ -0,0 +1,49 @@
+package com.jerry.workoutapp.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "Users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", columnDefinition = "INTEGER")
+    private long userId;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password_hash")
+    private String passwordHash;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    // FIX LATER WHEN WORKOUT ENTITY IS DONE
+    // Relationships - One user can have many workouts
+    //@OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    //private List<Workout> workouts;
+
+
+    public User() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public User(String email, String passwordHash, LocalDateTime createdAt) {
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPasswordHash() { return passwordHash; }
+    public void setPasswordHash(String passwordHash) { this.passwordHash = passwordHash; }
+
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+}
