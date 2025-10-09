252de8f72e6827a762249a44b84457bfc5736a9a
Jerry <you@example.com>
2025-10-09 14:53:00 +0200

    added number of exercises to workout

--- src/main/java/com/jerry/workoutapp/dto/WorkoutResponse.java
+++ src/main/java/com/jerry/workoutapp/dto/WorkoutResponse.java
@@ -8,24 +8,23 @@ public class WorkoutResponse {
     private String name;
     private Long userId;
     private LocalDateTime createdAt;
+    private Integer exerciseCount;
     private List<WorkoutExerciseResponse> exercises;
 
-    public WorkoutResponse() {
-    }
-
-    public WorkoutResponse(Long workoutId, String name, Long userId,
-                           LocalDateTime createdAt, List<WorkoutExerciseResponse> exercises) {
+    public WorkoutResponse(Long workoutId, String name, Long userId, LocalDateTime createdAt,
+                           Integer exerciseCount, List<WorkoutExerciseResponse> exercises) {
         this.workoutId = workoutId;
         this.name = name;
         this.userId = userId;
         this.createdAt = createdAt;
+        this.exerciseCount = exerciseCount;
         this.exercises = exercises;
     }
 
+    // Getters and Setters
     public Long getWorkoutId() {
         return workoutId;
     }
-
     public void setWorkoutId(Long workoutId) {
         this.workoutId = workoutId;
     }
@@ -33,7 +32,6 @@ public class WorkoutResponse {
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
@@ -41,7 +39,6 @@ public class WorkoutResponse {
     public Long getUserId() {
         return userId;
     }
-
     public void setUserId(Long userId) {
         this.userId = userId;
     }
@@ -49,15 +46,20 @@ public class WorkoutResponse {
     public LocalDateTime getCreatedAt() {
         return createdAt;
     }
-
     public void setCreatedAt(LocalDateTime createdAt) {
         this.createdAt = createdAt;
+    }
+
+    public Integer getExerciseCount() {
+        return exerciseCount;
+    }
+    public void setExerciseCount(Integer exerciseCount) {
+        this.exerciseCount = exerciseCount;
     }
 
     public List<WorkoutExerciseResponse> getExercises() {
         return exercises;
     }
-
     public void setExercises(List<WorkoutExerciseResponse> exercises) {
         this.exercises = exercises;
     }
